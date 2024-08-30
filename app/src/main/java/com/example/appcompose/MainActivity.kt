76b449e9145d27b9f555d7da7fa9d9cfdd43d526
Farasat Novruzov <f.novruzov@Expressbank.az>
2024-08-30 19:02:58 +0400

    lesson 5

--- app/src/main/java/com/example/appcompose/MainActivity.kt
+++ app/src/main/java/com/example/appcompose/MainActivity.kt
@@ -1,53 +1,37 @@
 package com.example.appcompose
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.appcompose.ui.theme.AppComposeTheme
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
-        setContent {
-            Surface(color = MaterialTheme.colorScheme.background) {
-                ImageGrid()
 
-
-            }
+//        setContent {
+//            Surface(color = MaterialTheme.colorScheme.background) {
+//                ImageGrid()
+//            }
 
 //            Row(
 //                modifier = Modifier
@@ -64,74 +48,201 @@ class MainActivity : ComponentActivity() {
 //            }
 
 //-------------------------------------------------------------------------------------
-            Column(
-                modifier = Modifier
-//                        .background(Color.Green)
-                    .fillMaxHeight(1f)
-                    .fillMaxWidth(1f)
-//                    .fillMaxWidth(0.8f)
-//                    .width(400.dp)
-//                    .requiredWidth(400.dp)
-//                    .padding(20.dp)
-//                    .padding(top = 20.dp)
-                    .border(5.dp,Color.Red)
-                    .padding(3.dp)
-                    .border(3.dp,Color.Blue)
-            ) {
-//                Text(text = "Hello", modifier = Modifier
-//                    .offset(0.dp,20.dp))
-                Text(text = "Hello",modifier = Modifier
-                    .border(5.dp,Color.Red)
-                    .padding(5.dp)
-                    .padding(top = 50.dp)
-                    .padding(start = 50.dp)
-                    .border(5.dp,Color.Blue)
-                    .clickable {  }
-                    .size(66.dp)
-                )
-                Spacer(modifier = Modifier.height(100.dp))
-                Text(text = "World")
-            }
-        }
-    }
+//            Column(
+//                modifier = Modifier
+////                        .background(Color.Green)
+//                    .fillMaxHeight(1f)
+//                    .fillMaxWidth(1f)
+////                    .fillMaxWidth(0.8f)
+////                    .width(400.dp)
+////                    .requiredWidth(400.dp)
+////                    .padding(20.dp)
+////                    .padding(top = 20.dp)
+//                    .border(5.dp,Color.Red)
+//                    .padding(3.dp)
+//                    .border(3.dp,Color.Blue)
+//            ) {
+////                Text(text = "Hello", modifier = Modifier
+////                    .offset(0.dp,20.dp))
+//                Text(text = "Hello",modifier = Modifier
+//                    .border(5.dp,Color.Red)
+//                    .padding(5.dp)
+//                    .padding(top = 50.dp)
+//                    .padding(start = 50.dp)
+//                    .border(5.dp,Color.Blue)
+//                    .clickable {  }
+//                    .size(66.dp)
+//                )
+//                Spacer(modifier = Modifier.height(100.dp))
+//                Text(text = "World")
+//            }
 
+//            ----------------------
 
+//            val painter = painterResource(id = R.drawable.philosophers)
+//            val title = "The Great muslims"
+//            val description = "Why were great Islamic scholars regarded as infidels?"
+//            Box(
+//                modifier = Modifier
+//                    .fillMaxWidth(0.5f)
+//                    .padding(16.dp)
+//            ) {
+//                ImageCard(painter = painter, title = title, contentDescription = description)
+//            }
 
+        //--------------------------
 
-    @Composable
-    fun ImageGrid() {
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(3), // Adjust the number of columns as needed
-            modifier = Modifier.padding(8.dp)
-        ) {
-            items(12) { index -> // Replace with your list of images
-                GridItem(imageResId = getImageResId(index))
-            }
-        }
-    }
-
-    @Composable
-    fun GridItem(imageResId: Int) {
-        Image(
-            painter = painterResource(id = imageResId),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .padding(4.dp)
-                .size(100.dp) // Adjust the size as needed
-                .background(Color.Gray, shape = RoundedCornerShape(8.dp))
+        val fontFamily = FontFamily(
+            Font(R.font.lexend_bold, FontWeight.Bold),
+            Font(R.font.lexend_light, FontWeight.Light),
+            Font(R.font.lexend_thin, FontWeight.Thin),
         )
-    }
+//        val uriHandler = LocalUriHandler.current
+        val annotatedString = buildAnnotatedString {
+            append("This is an ")
 
-    fun getImageResId(index: Int): Int {
-        // Return the correct drawable resource ID for the given index
-        return when (index % 4) {
-            0 -> R.drawable.ic_launcher_foreground
-            1 -> R.drawable.ic_launcher_foreground
-            2 -> R.drawable.ic_launcher_foreground
-            else -> R.drawable.ic_launcher_foreground
+            // Style "Annotated" differently
+            withStyle(style = SpanStyle(color = Color.Red, fontWeight = FontWeight.Bold)) {
+                append("Annotated")
+            }
+
+            append(" string. ")
+
+            // Add clickable annotation
+            pushStringAnnotation(tag = "URL", annotation = "https://www.example.com")
+            withStyle(
+                style = SpanStyle(
+                    color = Color.Blue,
+                    textDecoration = TextDecoration.Underline
+                )
+            ) {
+                append("Click here")
+            }
+            pop()
+
+            append(" to learn more.")
+        }
+
+        setContent {
+
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0XFF101010))
+            ) {
+                Text(
+//                    text = "Jetpack Compose",
+                    text = annotatedString,
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .clickable {
+                            // Handle click events on annotations
+                            val annotations = annotatedString.getStringAnnotations(
+                                "URL",
+                                0,
+                                annotatedString.length
+                            )
+                            annotations.firstOrNull()?.let {
+                                // Example: Open URL in browser
+                                println("Clicked URL: ${it.item}")
+//                                uriHandler.openUri(it.item)
+                            }
+                        }
+                )
+            }
+
+
         }
     }
+
+
+    //-------------------------
+//    @Composable
+//    fun ImageCard(
+//        painter: Painter,
+//        contentDescription: String,
+//        title: String,
+//        modifier: Modifier = Modifier,
+//    ) {
+//        Card(
+//            modifier = modifier
+//                .fillMaxWidth()
+//                .height(250.dp),  // Set a fixed height for the card
+//            shape = RoundedCornerShape(15.dp),
+//            elevation = CardDefaults.cardElevation(10.dp)
+//        ) {
+//            Box(modifier = Modifier.fillMaxSize()) {
+//                Image(
+//                    painter = painter,
+//                    contentDescription = contentDescription,
+//                    contentScale = ContentScale.Crop,
+//                    modifier = Modifier.fillMaxSize()
+//                )
+//                Box(
+//                    modifier = Modifier
+//                        .fillMaxSize()
+//                        .background(
+//                            Brush.verticalGradient(
+//                                colors = listOf(
+//                                    Color.Transparent,
+//                                    Color.Black
+//                                ),
+//                                startY = 300f
+//                            )
+//                        )
+//                )
+//                Box(
+//                    modifier = Modifier
+//                        .fillMaxSize()
+//                        .padding(12.dp),
+//                    contentAlignment = Alignment.BottomStart
+//                ) {
+//                    Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+//                }
+//            }
+//        }
+//    }
+
+
+    //---------------------------------
+//    @Composable
+//    fun ImageGrid() {
+//        LazyVerticalGrid(
+//            columns = GridCells.Fixed(3), // Adjust the number of columns as needed
+//            modifier = Modifier.padding(8.dp)
+//        ) {
+//            items(12) { index -> // Replace with your list of images
+//                GridItem(imageResId = getImageResId(index))
+//            }
+//        }
+//    }
+//
+//    @Composable
+//    fun GridItem(imageResId: Int) {
+//        Image(
+//            painter = painterResource(id = imageResId),
+//            contentDescription = null,
+//            contentScale = ContentScale.Crop,
+//            modifier = Modifier
+//                .padding(4.dp)
+//                .size(100.dp) // Adjust the size as needed
+//                .background(Color.Gray, shape = RoundedCornerShape(8.dp))
+//        )
+//    }
+//
+//    fun getImageResId(index: Int): Int {
+//        // Return the correct drawable resource ID for the given index
+//        return when (index % 4) {
+//            0 -> R.drawable.ic_launcher_foreground
+//            1 -> R.drawable.ic_launcher_foreground
+//            2 -> R.drawable.ic_launcher_foreground
+//            else -> R.drawable.ic_launcher_foreground
+//        }
+//    }
 
 
 }
