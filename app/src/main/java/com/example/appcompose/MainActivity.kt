d7bc87118377e4dc9ed1d6f0a8692655f95e8cd7
FarasatN <farasatnovruzov@gmail.com>
2024-09-18 11:15:38 +0400

    deployed from home

--- app/src/main/java/com/example/appcompose/MainActivity.kt
+++ app/src/main/java/com/example/appcompose/MainActivity.kt
@@ -1,10 +1,27 @@
 package com.example.appcompose
 
+import android.health.connect.datatypes.units.Percentage
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateSizeAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -13,8 +30,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -304,15 +330,91 @@ class MainActivity : ComponentActivity() {
         //Simple Animations
 
 
+//        setContent{
+//            var sizeState by remember { mutableStateOf(0.125f) }
+//            val size by animateFloatAsState(targetValue = sizeState,
+////                tween(durationMillis = 2000,
+////                    delayMillis = 300,
+////                    easing = LinearOutSlowInEasing)
+//
+//                spring(Spring.DampingRatioHighBouncy)
+//
+////                keyframes {
+////                    durationMillis = 2000 // Total duration of the animation
+////                    sizeState at 0 with LinearEasing // Reach 100.dp at 500ms
+////                    sizeState*1.5f at 1000 with FastOutLinearInEasing // Reach 200.dp at 1000ms
+////                    sizeState*2f at 1500 // Reach 250.dp at 1500ms
+////                    // Finally reach the target value at 2000ms
+////                }
+//
+//            )
+//
+//
+////            var isGrowing by remember { mutableStateOf(true) } // Track if the Box is growing or shrinking
+//
+//            val infiniteTransition = rememberInfiniteTransition()
+//            val color by infiniteTransition.animateColor(
+//                initialValue = Color.Yellow,
+//                targetValue = Color.Green,
+//                animationSpec = infiniteRepeatable(
+//                    tween(
+//                        durationMillis = 2000,
+//                    delayMillis = 300,
+//                    easing = LinearOutSlowInEasing
+//                    ),
+//                    repeatMode = RepeatMode.Reverse
+//                ), label = ""
+//            )
+//
+//            Box(modifier = Modifier
+////                .size(sizeState)
+////                .size(size)
+//                .fillMaxSize(size)
+////                .background(Color.Yellow),
+//                .background(color),
+//                contentAlignment = Alignment.Center) {
+//                Button(
+////                    modifier = Modifier.fillMaxSize(size/1.1f),
+//                    modifier = Modifier.size((size*200).dp),
+//                    onClick = {
+//                    sizeState = if (sizeState < 1f) sizeState * 1.1f else 0.125f // Grow until full screen, then reset
+//
+//
+////                    if (isGrowing) {
+////                        // Grow the Box until it reaches full screen size
+////                        if (sizeState < 1f) {
+////                            sizeState *= 1.1f
+////                        } else {
+////                            // Once full size is reached, start shrinking on next click
+////                            isGrowing = false
+////                        }
+////                    } else {
+////                        // Shrink the Box back to the initial size
+////                        if (sizeState > 0.125f) {
+////                            sizeState /= 1.1f
+////                        } else {
+////                            // Once initial size is reached, start growing again on next click
+////                            isGrowing = true
+////                        }
+////                    }
+//
+//                }) {
+//                    Text(text = "Button with: $size", color = Color.Cyan, fontSize = (size*10).sp)
+////                    Text(text = "Two color", color = Color.Red)
+//                }
+//            }
+//        }
+
+
+
+        //----------------------------------
+        //Circular Progress Bar
         setContent{
-            var sizeState by remember { mutableStateOf(200.dp) }
-            Box(modifier = Modifier.size(200.dp)
-                .background(Color.Red),
-                contentAlignment = Alignment.Center) {
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Button with", color = Color.Magenta)
-                    Text(text = "Two color", color = Color.Red)
-                }
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ){
+                CircularProgressBar(percentage = 0.99f, number = 100)
             }
         }
 
@@ -321,6 +423,56 @@ class MainActivity : ComponentActivity() {
 
 
 
+    //------------------
+    //Circular Progress bar
+    @Composable
+    fun CircularProgressBar(
+        percentage: Float,
+        number: Int,
+        fontSize: TextUnit = 28.sp,
+        radius: Dp = 50.dp,
+        color: Color = Color.Green,
+        strokeWith: Dp = 8.dp,
+        animDuration: Int = 1000,
+        animDelay: Int = 0,
+
+        ){
+        var animationPlayed by remember {
+            mutableStateOf(false)
+        }
+
+        val curPercentage = animateFloatAsState(
+            targetValue = if(animationPlayed) percentage else 0f,
+            animationSpec = tween(
+                durationMillis = animDuration,
+                delayMillis = animDelay
+            )
+        )
+        LaunchedEffect(key1 = true) {
+            animationPlayed = true
+        }
+
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.size( radius*2f),
+        ){
+            Canvas(modifier = Modifier.size(radius*2f)){
+                drawArc(
+                    color = color,
+                    -90f,
+                    360*curPercentage.value,
+                    useCenter = false,
+                    style = Stroke(strokeWith.toPx(),cap = StrokeCap.Round)
+                )
+            }
+            Text(
+                text = (curPercentage.value*number).toInt().toString(),
+                color = Color.DarkGray,
+                fontSize = fontSize,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
 
 
 
